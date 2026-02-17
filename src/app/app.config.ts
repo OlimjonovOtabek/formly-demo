dc385649493914ed2f46567770f72ee97495cf89
olimjonovh <almjnvfsystem@gmail.com>
2026-02-17 18:22:17 +0500

    chore: add PrimeNG and PrimeUI themes, update app configuration and styles
    - Added dependencies for PrimeNG and PrimeUI themes in package.json.
    - Updated app.config.ts to include PrimeNG configuration with Aura theme.
    - Removed placeholder content from app.html and added a router outlet.
    - Updated styles.css to import TailwindCSS and TailwindCSS-PrimeUI.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.layout-dark',
+          cssLayer: false
+        }
+      }
+    })
   ]
 };
